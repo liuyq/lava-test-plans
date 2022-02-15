f0e06ff5ca740c656ff9a0687b0a4c48f7904ff9
Yongqin Liu <yongqin.liu@linaro.org>
2022-02-15 21:58:44 +0800

    android: add testcase templates for x15 and hikey960
    with the docker method used for lava jobs.
    needs to specify the --testplan-path to projects/lkft-android,
    and --testplan-device-path to devices/android/,
    for the submit_for_testing.py script to be run.
    
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- selftests.sh
+++ selftests.sh
@@ -1,27 +1,54 @@
 #!/bin/bash
 
-function run_device_selftest()
-{
-    ./submit_for_testing.py --variables variables.ini --device-type $1 --dry-run --test-case $2
-    echo $?
+function call_submit_for_testing_py(){
+    local device_type="$1"
+    local test_case="$2"
+    local f_variables_ini="$3"
+    local dir_testplan_path="$4"
+    local dir_testplan_device_path="$5"
+
+    opt_testplan_path=""
+    opt_testplan_device_path=""
+    [ -n "${dir_testplan_path}" ] && opt_testplan_path="--testplan-path ${dir_testplan_path}"
+    [ -n "${dir_testplan_device_path}" ] && opt_testplan_device_path="--testplan-device-path ${dir_testplan_device_path}"
+
+    ./submit_for_testing.py \
+                --device-type "${device_type}" \
+                --test-case "${test_case}" \
+                --variables "${f_variables_ini}" \
+                ${opt_testplan_path} \
+                ${opt_testplan_device_path} \
+                --dry-run
+    return $?
 }
 
-exit_code=0
-for DEVICE_TYPE in ./devices/*
-do
-    for TEST in ./testcases/*.yaml
-    do
-        if [ -d $DEVICE_TYPE ]
-        then
-            for NESTED_DEVICE_TYPE in $DEVICE_TYPE/*
-            do
-                device_exit_code=$(run_device_selftest $(basename $DEVICE_TYPE)/$(basename $NESTED_DEVICE_TYPE) $(basename $TEST))
-                if [ $device_exit_code -gt $exit_code ]; then exit_code=$device_exit_code; fi
-            done
+function test_for_templates(){
+    local exit_code=0
+    for F_DEVICE_TYPE in $(find ./devices/ -type f); do
+        if echo "${F_DEVICE_TYPE}" | grep -q "./devices/android/"; then
+            DEVICE_TYPE=$(echo "${F_DEVICE_TYPE}" | sed 's|./devices/android/||')
+            if [ "X${DEVICE_TYPE}" = "Xdevice-base" ];then
+                # device_base is the generic template files for all devices
+                continue
+            fi
+            DIR_TESTCASES="./testcases/android"
+            DIR_DEVICES="./devices/android"
+            F_VARIABLES_INI="variables-android.ini"
         else
-            device_exit_code=$(run_device_selftest $(basename $DEVICE_TYPE) $(basename $TEST))
-            if [ $device_exit_code -gt $exit_code ]; then exit_code=$device_exit_code; fi
+            DEVICE_TYPE=$(echo "${F_DEVICE_TYPE}" | sed 's|./devices/||')
+            DIR_TESTCASES="./testcases"
+            DIR_DEVICES="./devices"
+            F_VARIABLES_INI="variables.ini"
         fi
+
+        for F_PATH_TESTCASE in $(find "${DIR_TESTCASES}" -maxdepth 1 -type f -name '*.yaml'); do
+            F_NAME_TESTCASE=$(basename "${F_PATH_TESTCASE}")
+            call_submit_for_testing_py "${DEVICE_TYPE}" "${F_NAME_TESTCASE}" "${F_VARIABLES_INI}" "${DIR_TESTCASES}" "${DIR_DEVICES}"
+            [ $? -gt $exit_code ] && exit_code=$?
+        done
     done
-done
-exit $exit_code
+
+    return $exit_code
+}
+
+test_for_templates
